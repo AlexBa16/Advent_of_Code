0a91d8c1a113d31737a42d0c06a5eae1b5a9cf4b
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2025-12-02 16:32:40 +0100

    add Time measurement

--- year_2025/main_2025.ts
+++ year_2025/main_2025.ts
@@ -3,14 +3,19 @@ import {day01_part1, day01_part2} from "./days/day01.ts";
 import {day02_part1, day02_part2} from "./days/day02.ts";
 import {day03_part1, day03_part2} from "./days/day03.ts";
 
+const startTime = performance.now();
+
 // console.log("===== Day01 =====");
 // console.log('Part1:', day01_part1(readFileAsArray(2025, 1)));
 // console.log('Part2:', day01_part2(readFileAsArray(2025, 1)));
-
+//
 // console.log("===== Day02 =====");
 // console.log('Part1:', day02_part1(readFileAsString(2025, 2)));
 // console.log('Part2:', day02_part2(readFileAsString(2025, 2)));
-
+//
 console.log("===== Day03 =====");
 console.log('Part1:', day03_part1(readFileAsString(2025, 3)));
 console.log('Part2:', day03_part2(readFileAsString(2025, 3)));
+
+console.log("\n==== Runtime ====");
+console.log(Number((performance.now() - startTime).toFixed(5)), "ms");
