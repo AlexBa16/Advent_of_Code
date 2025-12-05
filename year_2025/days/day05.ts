6ff380efc745318563028563328d8471a56a696d
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2025-12-05 06:08:32 +0100

    day5 frist try

--- year_2025/days/day05.ts
+++ year_2025/days/day05.ts
@@ -1,6 +1,19 @@
 function day05_part1(lines: string[]): number {
     let output: number = 0;
-
+    let fresh: number[] = [];
+    for (const line of lines) {
+        console.log(line)
+        if (line.split('-')[1] !== undefined) {
+            let from = Number(line.split('-')[0]);
+            let to = Number(line.split('-')[1]);
+            for (let i = from; i <= to; i++) {
+                fresh.push(i);
+            }
+        } else {
+            console.log(fresh)
+            if (fresh.includes(Number(line))) output++;
+        }
+    }
     return output;
 }
 
