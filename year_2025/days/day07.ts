b4c58d6c964280597471687158f06138bc45e09c
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2025-12-07 13:26:31 +0100

    day7 part2 first try

--- year_2025/days/day07.ts
+++ year_2025/days/day07.ts
@@ -4,26 +4,43 @@ let part1Counter: number = 0;
 
 function day07_part1(lines: string[]): number {
     let map = char2DArr(lines);
-    search(map, map[0]!.indexOf("S"), 1)!;
+    part1RecursiveSearch(map, map[0]!.indexOf("S"), 1);
     return part1Counter;
 }
 
-function search(map: string[][], x: number, y: number): void {
+function part1RecursiveSearch(map: string[][], x: number, y: number): void {
     if (y >= map.length) return;
     if (map[y]![x] === '^') {
-        search(map, x - 1, y + 1);
-        search(map, x + 1, y + 1);
+        part1RecursiveSearch(map, x - 1, y + 1);
+        part1RecursiveSearch(map, x + 1, y + 1);
         part1Counter++;
     } else if (map[y]![x] === '.') {
         map[y]![x] = '|';
-        search(map, x, y + 1);
+        part1RecursiveSearch(map, x, y + 1);
     }
 }
 
-function day07_part2(lines: string[]): number {
-    let output: number = 0;
+let part2Counter: number = 0;
 
-    return output;
+function day07_part2(lines: string[]): number {
+    let map = char2DArr(lines);
+    part2RecursiveSearch(map, map[0]!.indexOf("S"), 1);
+    return part2Counter;
+}
+
+function part2RecursiveSearch(map: string[][], x: number, y: number): void {
+    if (y >= map.length) {
+        part2Counter++;
+        return;
+    }
+    if (map[y]![x] === '^') {
+        part2RecursiveSearch(map, x - 1, y + 1);
+        part2RecursiveSearch(map, x + 1, y + 1);
+        // part2Counter++;
+    } else if (map[y]![x] === '.') {
+        // map[y]![x] = '|';
+        part2RecursiveSearch(map, x, y + 1);
+    }
 }
 
 export {day07_part1, day07_part2};
