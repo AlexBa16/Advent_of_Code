b74191cbfad21c9f7ec2cb7fdd23628f7b3a7e9c
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2025-12-07 13:04:10 +0100

    day7 part1

--- year_2025/days/day07.ts
+++ year_2025/days/day07.ts
@@ -1,7 +1,23 @@
-function day07_part1(lines: string[]): number {
-    let output: number = 0;
+import {char2DArr} from "../../utils/helpers.ts";
 
-    return output;
+let part1Counter: number = 0;
+
+function day07_part1(lines: string[]): number {
+    let map = char2DArr(lines);
+    search(map, map[0]!.indexOf("S"), 1)!;
+    return part1Counter;
+}
+
+function search(map: string[][], x: number, y: number): void {
+    if (y >= map.length) return;
+    if (map[y]![x] === '^') {
+        search(map, x - 1, y + 1);
+        search(map, x + 1, y + 1);
+        part1Counter++;
+    } else if (map[y]![x] === '.') {
+        map[y]![x] = '|';
+        search(map, x, y + 1);
+    }
 }
 
 function day07_part2(lines: string[]): number {
