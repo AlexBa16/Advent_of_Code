16393b4e84746f67b5f6e1295315a806bfe797f9
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2025-12-09 15:21:55 +0100

    day9 part2 IV

--- year_2025/days/day09.ts
+++ year_2025/days/day09.ts
@@ -27,7 +27,6 @@ function day09_part2(lines: string[]): number {
         if (numbers[0]! > width) width = numbers[0]!;
         if (numbers[1]! > height) height = numbers[1]!;
     }
-    // let map: string[][] = Array.from({length: height + 1}, () => new Array(width + 1).fill('.'));
 
     const isTileValid = (x: number, y: number) =>
         redTiles.some(p => p.x === x && p.y === y) ||
@@ -38,7 +37,6 @@ function day09_part2(lines: string[]): number {
 
     for (const line of lines) {
         let [x, y]: number[] = line.split(',').map(Number);
-        // map[y!]![x!] = '#';
         redTiles.push({x: x!, y: y!});
     }
     height++;
@@ -55,11 +53,7 @@ function day09_part2(lines: string[]): number {
             let endY = Math.max(pointA.y, pointB.y);
 
             for (let y = startY; y <= endY; y++) {
-                if (!isTileValid(pointA.x, y)) {
-                    // if (map[y]![pointA.x] === '.') {
-                    greenTiles.push({x: pointA.x, y: y});
-                    // map[y]![pointA.x] = 'X';
-                }
+                if (!isTileValid(pointA.x, y)) greenTiles.push({x: pointA.x, y: y});
             }
         }
         // Horizontal line
@@ -68,24 +62,14 @@ function day09_part2(lines: string[]): number {
             let endX = Math.max(pointA.x, pointB.x);
 
             for (let x = startX; x <= endX; x++) {
-                if (!isTileValid(x, pointA.y)) {
-                    // if (map[pointA.y]![x] === '.') {
-                    greenTiles.push({x: x, y: pointA.y});
-                    // map[pointA.y]![x] = 'X';
-                }
+                if (!isTileValid(x, pointA.y)) greenTiles.push({x: x, y: pointA.y});
             }
         }
     }
 
     for (let y = 0; y < height; y++) {
         for (let x = 0; x < width; x++) {
-            // let point = map[y]![x]!;
-            if (!isTileValid(x, y)
-                // if (point === '.'
-                && checkIfInsideOfWall(x, y)) {
-                greenTiles.push({x, y});
-                // map[y]![x] = 'X';
-            }
+            if (!isTileValid(x, y) && checkIfInsideOfWall(x, y)) greenTiles.push({x, y});
         }
     }
 
@@ -94,7 +78,6 @@ function day09_part2(lines: string[]): number {
         // Check left wall
         for (let lx = x - 1; lx >= 0; lx--) {
             if (isTileValid(lx, y)) {
-                // if (map[y]![lx] === '#' || map[y]![lx] === 'X') {
                 leftWall = true;
                 break;
             }
@@ -102,7 +85,6 @@ function day09_part2(lines: string[]): number {
         // Check right wall
         for (let rx = x + 1; rx < width; rx++) {
             if (isTileValid(rx, y)) {
-                // if (map[y]![rx] === '#' || map[y]![rx] === 'X') {
                 rightWall = true;
                 break;
             }
@@ -110,7 +92,6 @@ function day09_part2(lines: string[]): number {
         // Check top wall
         for (let ty = y - 1; ty >= 0; ty--) {
             if (isTileValid(x, ty)) {
-                // if (map[ty]![x] === '#' || map[ty]![x] === 'X') {
                 topWall = true;
                 break;
             }
@@ -118,7 +99,6 @@ function day09_part2(lines: string[]): number {
         // Check bottom wall
         for (let by = y + 1; by < height; by++) {
             if (isTileValid(x, by)) {
-                // if (map[by]![x] === '#' || map[by]![x] === 'X') {
                 bottomWall = true;
                 break;
             }
@@ -126,27 +106,15 @@ function day09_part2(lines: string[]): number {
         return leftWall && rightWall && topWall && bottomWall;
     }
 
-    greenTiles.sort((a, b) => a.x - b.x);
-
-    console.log(greenTiles);
-    // console.log(displayAsMap(map));
-
     let biggestArea: number = 0;
     for (let i = 0; i < redTiles.length; i++) {
         let pointA = redTiles[i]!;
         for (let j = 0; j < redTiles.length; j++) {
             let pointB = redTiles[j]!;
             if (i === j) continue;
-            let startX = Math.min(pointA.x, pointB.x);
-            let startY = Math.min(pointA.y, pointB.y);
-            let endX = Math.max(pointA.x, pointB.x);
-            let endY = Math.max(pointA.y, pointB.y);
-            let xDiff = pointB.x - pointA.x + 1;
-            let yDiff = pointB.y - pointA.y + 1;
-
             let valid = true;
-            for (let y = startY; y <= endY; y++) {
-                for (let x = startX; x <= endX; x++) {
+            for (let y = Math.min(pointA.y, pointB.y); y <= Math.max(pointA.y, pointB.y); y++) {
+                for (let x = Math.min(pointA.x, pointB.x); x <= Math.max(pointA.x, pointB.x); x++) {
                     if (!(redTiles.some(p => p.x === x && p.y === y) ||
                         greenTiles.some(p => p.x === x && p.y === y))) {
                         valid = false;
@@ -155,10 +123,7 @@ function day09_part2(lines: string[]): number {
                 }
                 if (!valid) break;
             }
-
-
-            // if (greenTiles.indexOf({x, y}) === -1) {
-            if (valid) biggestArea = Math.max(biggestArea, xDiff * yDiff);
+            if (valid) biggestArea = Math.max(biggestArea, (pointB.x - pointA.x + 1) * (pointB.y - pointA.y + 1));
         }
     }
     return biggestArea;
