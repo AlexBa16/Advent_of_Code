c36deeb86ae5486a9b1116d80989abceb6d4205d
AlexBa16 <alexbachinger56+GitHub@gmail.com>
2025-12-27 14:02:08 +0100

    day10 part1 I buttons to binary

--- year_2025/days/day10.ts
+++ year_2025/days/day10.ts
@@ -1,55 +1,29 @@
-import {readFileAsString} from "../../utils/files.ts";
-
 function day10_part1(lines: string[]): number {
     let output: number = 0;
     for (let line of lines) {
         const parts = line.split(" ");
 
-        const lights: boolean[] = parts[0]!.slice(1, -1).split("").map(l => l !== ".");
+        const diagramLights: number[] = parts[0]!.slice(1, -1).split("").map(l => l === "." ? 0 : 1);
+        const lights: number[] = new Array(diagramLights.length).fill(0).map(() => 0);
         const buttons: number[][] = parts.slice(1, -1).map(b => b.replace(/[()]/g, "").split(",").map(n => Number(n)));
 
-        function combinations<T>(buttons: T[]): T[][] {
-            const result: T[][] = [];
-
-            function helper(start: number, combo: T[]) {
-                for (let i = start; i < buttons.length; i++) {
-                    const newCombo = [...combo, buttons[i]!];
-                    result.push(newCombo);
-                    helper(i + 1, newCombo);
-                }
-            }
-
-            helper(0, []);
-            return result;
+        for (let i = 0; i < buttons.length; i++) {
+            let newButton: number[] = new Array(lights.length).fill(0);
+            for (let j = 0; j < buttons[i]!.length; j++) newButton[buttons[i]![j]!] = 1;
+            buttons[i] = newButton;
         }
 
-        function calcValue(buttons: number[][]): number {
-            let value: number = 0;
-            for (const button of buttons) {
-                for (let i = 0; i < button.length; i++) {
-                    lights[button[i]!] = !lights[button[i]!];
-                    if (!lights.includes(false)) {
-                        value++;
-                        break;
-                    }
+        const patternNr = Math.pow(2, buttons.length);
+        let smallestNrOfPresses: number = -1;
 
-                }
-            }
-            return value;
-        }
 
-        const results: Map<string, number> = new Map();
-        const allCombos = combinations(buttons);
 
-        for (const combo of allCombos) {
-            results.set(JSON.stringify(combo), calcValue(combo));
-            // console.log(combo);
-        }
+        // XOR -> toggle
 
-        console.log(results);
-        // console.log(lights);
-        // console.log(buttons);
-
+        // console.log(patternNr)
+        // console.log(diagramLights)
+        console.log(buttons)
+        // console.log(lights)
     }
     return output;
 }
